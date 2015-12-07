f0197b8d567da19a73af58f69dbcff762b8a400f
CaymanSeb <sebastian.bluefire@freenet.de>
2015-12-07 11:12:04 +0100

    Create Übung4-1.cpp

---
+++ Übung4-1.cpp
@@ -0,0 +1,117 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+///Hauptklasse///
+class Instrument {
+
+public:
+	virtual void bewegeLuft() = 0;
+
+};
+
+///Unterklassen///
+///1///
+class Idiophone : public Instrument {
+
+};
+
+class Schlagidiophone : public Idiophone {
+
+};
+
+class Glockenspiel : public Schlagidiophone {
+
+public:
+	void bewegeLuft() override {
+		cout << "Pling.." << endl;
+	}
+};
+
+///2///
+class Membranophone : public Instrument {
+
+};
+
+class Reibtrommeln : public Membranophone {
+
+};
+
+class Felltrommel : public Reibtrommeln {
+
+public:
+	void bewegeLuft() override {
+		cout << "Rib, Rib.." << endl;
+	}
+};
+
+///3///
+class Chordophone : public Instrument {
+
+};
+
+class Zusammengesetzte_Chordophone : public Chordophone {
+
+};
+
+class Harfe : public Schlagidiophone {
+
+public:
+	void bewegeLuft() override {
+		cout << "Dwing.." << endl;
+	}
+};
+
+///4///
+class Aerophone : public Instrument {
+
+};
+
+class Freie_Aerophone : public Aerophone {
+
+};
+
+class Rohrbombe : public Freie_Aerophone {
+
+public:
+	void bewegeLuft() override {
+		cout << "Rumps, Bomm, Peng, Krach.." << endl;
+	}
+};
+
+///5///
+class Elektrophone : public Instrument {
+
+};
+
+class Keyboard : public Elektrophone {
+
+public:
+	void bewegeLuft() override {
+		cout << "Klimper, Klimp.." << endl;
+	}
+};
+
+
+
+int main()
+{
+	Glockenspiel *glöckchen = new Glockenspiel();
+	glöckchen->bewegeLuft();
+
+	Felltrommel *trommel = new Felltrommel();
+	trommel->bewegeLuft();
+
+	Harfe *harfe = new Harfe();
+	harfe->bewegeLuft();
+
+	Rohrbombe *bömbchen = new Rohrbombe();
+	bömbchen->bewegeLuft();
+
+	Keyboard *piano = new Keyboard();
+	piano->bewegeLuft();
+
+    return 0;
+}
+
